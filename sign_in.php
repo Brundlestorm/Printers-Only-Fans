f2826b3a7a5b03458fbf138037eec8f48c80fef0
Brundlestorm <matt.collins@1728Studios.com>
2024-01-10 17:21:57 -0700

    Update sign_in.php

--- sign_in.php
+++ sign_in.php
@@ -39,7 +39,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $_SESSION['email'] = $email;
 
             // Redirect to main page or dashboard
-            header("Location: mainpage.html");
+            header("Location: homepage.html");
             exit;
         } else {
             // Invalid password
