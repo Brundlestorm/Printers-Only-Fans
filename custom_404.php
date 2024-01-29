a52db54384126a4625b918c3f6570b9c3fe13d0e
Robert Greenwood <RobertGreenwood27@gmail.com>
2024-01-29 06:30:14 -0700

    include footer

--- custom_404.php
+++ custom_404.php
@@ -21,19 +21,6 @@
     </main>
     
     
-    <footer class="py-2 bg-zinc-800">
-        <div class="container mx-auto text-center container-mobile">
-            <div class="custom-banner mb-2">
-                <p class="text-sm">60% of all proceeds are donated to St. Jude Children's Research Hospital in the name of 1728Studios LLC.</p>
-                <p class="text-sm">If you prefer to donate directly, please visit their official site:</p>
-                <a href="https://www.stjude.org" target="_blank" class="text-blue-400 hover:text-blue-300 text-sm">stjude.org</a>
-            </div>
-            <div class="footer-links space-x-2 mb-2">
-                <a href="https://www.1728studios.com/about" target="_blank" class="hover:text-zinc-300 hover-effect text-sm">About</a>
-            </div>
-            <p class="text-sm">© Printers Only Fans Brought to you by 1728studios.com</p>
-        </div>
-    </footer>
-    <script src='background.js'></script>
+    <?php include 'footer.php'; ?>
 </body>
 </html>
