94a6ed0c8a1a0bb6b3e7bc4c423b517b21e340f8
Brundlestorm <matt.collins@1728Studios.com>
2024-04-10 11:06:46 -0600

    Create videodownloads3.php
    optional table deployment for listings

---
+++ videodownloads3.php
@@ -0,0 +1,64 @@
+<?php
+session_start(); // Start the session
+
+// Check if the user is not signed in
+if (!isset($_SESSION['user_id'])) {
+    // Redirect to the sign-in page
+    header("Location: signin");
+    exit;
+}
+
+?>
+
+<?php include 'header.php'; ?>
+
+<section class="flex justify-center items-center">
+    <video class="w-48 h-48 rounded-full border-4 border-zinc-400 ml-4 mt-4" loop autoplay playsinline muted>
+        <source src="logo.mp4" type="video/mp4">
+        <img src="logo.png" alt="Printers Only Fans Logo" class="w-48 h-48 rounded-full border-4 border-zinc-400 ml-4 mt-4">
+    </video>
+</section>
+
+<main>
+    <?php
+    $servername = "localhost";
+    $username = "octoprint";
+    $password = "Downloadmore1";
+    $dbname = "timelapse_db";
+
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "SELECT filename, filepath FROM videos ORDER BY filename"; // Add ORDER BY clause to alphabetize by filename
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if ($result->num_rows > 0) {
+        echo "<div class='columns'>";
+        $count = 0;
+        while($row = $result->fetch_assoc()) {
+            if ($count % 10 == 0 && $count != 0) {
+                echo "</div><div class='columns'>";
+            }
+            echo "<div class='column'>";
+            echo "<a href='serve_file?file=" . urlencode($row["filename"]) . "'>" . htmlspecialchars($row["filename"]) . "</a>";
+            echo "</div>";
+            $count++;
+        }
+        echo "</div>";
+    } else {
+        echo "0 results";
+    }
+
+    $stmt->close();
+    $conn->close();
+    ?>
+</main>
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
