5bf3b2d507daad3ec24f5e9da3107e70f9bef628
Brundlestorm <matt.collins@1728Studios.com>
2024-04-16 20:21:27 -0600

    Update videolibrary.php

--- videolibrary.php
+++ videolibrary.php
@@ -20,7 +20,7 @@ if (!isset($_SESSION['user_id'])) {
 </section>
 
 <main class="container mx-auto my-8 mt-36 flex flex-row items-center justify-center container-mobile">
-    <section class="text-center mt-8 mx-auto max-w-2xl">
+    <div class="text-center mt-8 mx-auto max-w-2xl">
         <h2 class="text-3xl font-bold mb-4">A word about downloads</h2>
         <p class="text-lg text-yellow-500">
             ""Welcome to the peculiar realm of Printers Only Fans, where the mundane meets the bizarre in a delightful dance of 3-D printing escapades. Here, amidst the tangled web of filament and the occasional intrusion of ill-placed supports, you'll find a treasure trove of raw footage straight from the belly of the printer.
@@ -31,8 +31,10 @@ This isn't your typical instructional guide or polished showcase. No, this is a 
 
 So join us, fellow adventurers, as we embark on this lighthearted romp through the quirky corners of 3-D printing. Together, let's revel in the joy of creativity, chuckle at the absurdity of it all, and perhaps, just perhaps, find a moment of whimsy in the most unexpected of places.""
         </p>
-    </section>
-    <a href="videodownloads" class="text-white bg-black px-4 py-2 rounded border-2 border-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition ease-in-out duration-300 mt-8">Video Downloads</a>
+    </div>
+    <div class="mt-8">
+        <a href="videodownloads" class="text-white bg-black px-4 py-2 rounded border-2 border-white hover:bg-blue-500 hover:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-opacity-50 transition ease-in-out duration-300">Video Downloads</a>
+    </div>
 </main>
 
 
