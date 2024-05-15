d41739902ed7296802c257ffc9ec5e402357349e
Brundlestorm <matt.collins@1728Studios.com>
2024-05-15 10:39:53 -0600

    Update homepage1.php
    updated API Key

--- homepage1.php
+++ homepage1.php
@@ -1,36 +1,50 @@
+<?php include 'header.php'; ?>
+   
+<script>
 document.addEventListener("DOMContentLoaded", function() {
   const checkPrinterStatus = () => {
-    console.log("Fetching printer status...");
     fetch('http://10.0.0.19/api/printer', {
       headers: {
-        'X-Api-Key': '5192AAC245424DCBA64E565E21450CDF'
+        'X-Api-Key': 'E969BA879E504CDCB183A6FF5E405FE0'
       }
     })
-    .then(response => {
-      console.log("API response received");
-      return response.json();
-    })
+    .then(response => response.json())
     .then(data => {
-      console.log("Data parsed:", data);
       const liveFeedButton = document.getElementById('liveFeedButton');
       if (!data.state.flags.operational) {
         liveFeedButton.onclick = function() {
-          window.location.href = '/our_apologies.php';
+          window.location.href = '/our_apologies.php';  // Redirect to maintenance page
         };
         liveFeedButton.innerText = 'Check Back Soon';
       } else {
-        liveFeedButton.setAttribute('href', '/webcam1');
+        liveFeedButton.href = '/webcam1';  // Link to live stream
         liveFeedButton.innerText = 'Live Stream';
       }
     })
-    .catch(error => {
-      console.error('Error fetching printer status:', error);
-      console.error(error);
-    });
+    .catch(error => console.error('Error fetching printer status:', error));
   };
 
+  // Check the printer status immediately and then every 5 minutes
   checkPrinterStatus();
   setInterval(checkPrinterStatus, 300000); // 300000 ms = 5 minutes
 });
+</script>
+
+<section class="flex justify-center items-center">
+    <video class="w-48 h-48 rounded-full border-4 border-zinc-400 ml-4 mt-4" loop autoplay playsinline muted>
+        <source src="logo.mp4" type="video/mp4">
+        <img src="logo.png" alt="Printers Only Fans Logo" class="w-48 h-48 rounded-full border-4 border-zinc-400 ml-4 mt-4">
+    </video>
+</section>
+
+<main class="container mx-auto my-8 mt-36 flex flex-row items-center justify-center container-mobile">
+    <a id="liveFeedButton" class="card-button card-button-mobile hover-effect mr-4">Live Stream</a>
+    <a href="videolibrary" class="card-button card-button-mobile hover-effect">Download Library</a>
+</main>
+
+<?php include 'footer.php'; ?>
+</body>
+</html>
+
 
 
