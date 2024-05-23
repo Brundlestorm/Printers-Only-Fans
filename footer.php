2fdaac883bee5ef05356809cb3a5cc472a018427
Brundlestorm <matt.collins@1728Studios.com>
2024-05-23 08:25:07 -0600

    Update footer.php
    removed St Judes info

--- footer.php
+++ footer.php
@@ -1,11 +1,7 @@
 <footer class="py-2 bg-zinc-800">
         <div class="container mx-auto text-center container-mobile">
             <div class="custom-banner mb-2">
-                <p class="text-sm">60% of all proceeds are donated to St. Jude Children's Research Hospital</p>
-                <p class="text-sm"> in the name of 1728Studios LLC.</p>
-                <p class="text-sm">If you prefer to donate directly, please visit their official site:</p>
-                <a href="https://www.stjude.org" target="_blank" class="text-blue-400 hover:text-blue-300 text-sm">stjude.org</a>
-            </div>
+                 </div>
             <div class="footer-links space-x-2 mb-2">
                 <a href="https://www.1728studios.com/about" target="_blank" class="hover:text-zinc-300 hover-effect text-sm">About 1728Studios</a>
             </div>
