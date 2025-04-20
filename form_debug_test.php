659c9d827aa9f5ab063b262d729074b16b5af243
Brundlestorm <matt.collins@1728Studios.com>
2025-04-19 18:49:45 -0600

    Create form_debug_test.php
    debugging test form

---
+++ form_debug_test.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head><title>Form Debug Test</title></head>
+<body>
+<h2>Form Test</h2>
+<form method="POST" action="form_debug_receiver.php">
+    <label>Email:</label><br>
+    <input type="email" name="email"><br><br>
+
+    <label>Password:</label><br>
+    <input type="password" name="password"><br><br>
+
+    <label>Confirm Password:</label><br>
+    <input type="password" name="confirm-password"><br><br>
+
+    <label>Address:</label><br>
+    <input type="text" name="address"><br><br>
+
+    <label>Print Suggestion:</label><br>
+    <input type="text" name="print-suggestion"><br><br>
+
+    <button type="submit">Test Submit</button>
+</form>
+</body>
+</html>
