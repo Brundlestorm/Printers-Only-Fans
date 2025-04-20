f49d174857e6244693c2dfe4f7aea5b7bca8bc22
Brundlestorm <matt.collins@1728Studios.com>
2025-04-19 19:21:55 -0600

    Update form_debug_test.php

--- form_debug_test.php
+++ form_debug_test.php
@@ -1,25 +1,4 @@
-<!DOCTYPE html>
-<html>
-<head><title>Form Debug Test</title></head>
-<body>
-<h2>Form Test</h2>
-<form method="POST" action="form_debug_receiver.php">
-    <label>Email:</label><br>
-    <input type="email" name="email"><br><br>
-
-    <label>Password:</label><br>
-    <input type="password" name="password"><br><br>
-
-    <label>Confirm Password:</label><br>
-    <input type="password" name="confirm-password"><br><br>
-
-    <label>Address:</label><br>
-    <input type="text" name="address"><br><br>
-
-    <label>Print Suggestion:</label><br>
-    <input type="text" name="print-suggestion"><br><br>
-
-    <button type="submit">Test Submit</button>
+<form action="form_debug_receiver.php" method="POST">
+  <input type="text" name="test" placeholder="Say something..." />
+  <input type="submit" value="Send" />
 </form>
-</body>
-</html>
